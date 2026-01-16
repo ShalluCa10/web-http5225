d49080b3d69501ae04bfb0bf0a363dffa56d9fdf
Shallu Sameera <shalluca1009@gmail.com>
2026-01-16 09:17:02 -0500

    week1
    web development http5225

---
+++ day1/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blockbuster</title>
+</head>
+<body>
+    <h1>Blockbuster</h1>
+    <p>Games list</p>
+    <?php echo 'Hello World'; ?>
+</body>
+</html>
